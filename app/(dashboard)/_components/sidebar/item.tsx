891f34c94005389aa63bed75ce738b0136f6beb7
Igor <igorkozakiewicz13@gmail.com>
2024-04-08 19:22:29 +0100

    worked on more options for orgs, created favourites and team boards

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
 
 interface ItemProps {
   id: string;
@@ -12,20 +13,31 @@ interface ItemProps {
 }
 
 export const Item = ({ id, name, imageUrl }: ItemProps) => {
-    const { organization } = useOrganization();
-    const { setActive } = useOrganizationList();
+  const { organization } = useOrganization();
+  const { setActive } = useOrganizationList();
 
-    const isActive = organization?.id === id;
+  const isActive = organization?.id === id;
 
-    const onClick = () => {
-        if (!setActive) return;
+  const onClick = () => {
+    if (!setActive) return;
 
-        setActive({ organization:id });
-    }
+    setActive({ organization: id });
+  };
 
-    return (
+  return (
     <div className="aspect-square relative">
-      <Image fill alt={name} src={imageUrl} onClick={onClick} className={cn("rounded-md cursor-pointer opacity-75 hover:opacity-100 transition", isActive && "opacity-100")} />
+      <Hint label={name} side="right" align="start" sideOffset={18}>
+        <Image
+          fill
+          alt={name}
+          src={imageUrl}
+          onClick={onClick}
+          className={cn(
+            "rounded-md cursor-pointer opacity-50 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
   );
 };
